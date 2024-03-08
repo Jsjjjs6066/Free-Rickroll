a925d39f32d29d5231006c954deb241c0caa231b
Jsjjjs6066 <136358293+Jsjjjs6066@users.noreply.github.com>
2024-03-08 14:46:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+// console.log(18446744073709551615);
+// console.log(18446744073709551615n);
+
+// let name = null;
+
+// while (true){
+//     name = prompt("Your name: ");
+//     if (! (name == '' || name == null)){
+//         break;
+//     }
+// }
+
+// document.getElementById("name").innerHTML = 'Your name is ' + name
