0e435e616f5c9f80ebcad2b817ccf2cec3a2463f
Ucenik <jsjjjs6066@outlook.com>
2024-03-14 13:27:37 +0100

    fix?

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById('r').onclick = function () {
+	location.href = 'rickroll.html'
+}
+document.getElementById('v').onclick = function () {
+	location.href = 'rickroll.html'
+}
+document.getElementById('mc').onclick = function () {
+	location.href = 'rickroll.html'
+}
+document.getElementById('g').onclick = function () {
+	location.href = 'rickroll.html'
+}
